5a197bf96f46fd76cb14d70545d1b2989b937758
higu1988 <ayaka.higuchi.0702@gmail.com>
2019-03-12 05:46:16 +0000

    fixbookaction

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,26 @@ class BooksController < ApplicationController
   def index
   end
 
+  def books
+  end
+
   def new
+    @book = Book.new
+  end
+  
+  def creat
+    book = Book.new(book_params)
+    book.save
+    # リダイレクト先は後でshow詳細ページに変更する
+    redirect_to books_path
   end
 
   def edit
   end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :category, :body)
+  end
+  
 end
