9fefee7c135933a22da0a8ef9a1ec1bd9b63f522
higu1988 <ayaka.higuchi.0702@gmail.com>
2019-03-14 04:30:31 +0000

    fixformfor

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,15 +9,22 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book = Book.new
   end
   
   def creat
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
   end
   
   private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
   
 end
