154ad56727ae746c3c19811f2ed2ddf8a26df92c
higu1988 <ayaka.higuchi.0702@gmail.com>
2019-03-14 04:01:58 +0000

    deleteformfor

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,22 +9,15 @@ class BooksController < ApplicationController
   end
 
   def new
-    @book = Book.new
   end
   
   def creat
-    book = Book.new(book_params)
-    book.save
-    # リダイレクト先は後でshow詳細ページに変更する
-    redirect_to books_path
   end
 
   def edit
   end
   
   private
-  def book_params
-    params.require(:book).permit(:title, :category, :body)
-  end
+
   
 end
